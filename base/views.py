1ca9e51ac046353453d9259b317281afa166fbf5
Spencer Richardson <sr23513@gmail.com>
2022-09-07 18:59:31 -0700

    setup models

--- base/views.py
+++ base/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
 
-def room(request):
-    return render(request, 'room.html')
+# rooms = [
+#     {'id':1, 'name':"Let's learn python!"},
+#     {'id':2, 'name':"Design with us"},
+#     {'id':3, 'name':"Frontend developers"},
+# ]
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html',context)
+
+def room(request, pk):
+    #get is for a unique identifying value
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    
+    return render(request, 'base/room.html', context)
