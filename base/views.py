d0893c922fecc126038c2f682a36835bceadc919
Spencer Richardson <sr23513@gmail.com>
2022-09-14 17:50:46 -0700

    added activity feed to home page

--- base/views.py
+++ base/views.py
@@ -75,8 +75,10 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
+    #Able to modify messages with object functions from Message
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html',context)
 
 def room(request, pk):
